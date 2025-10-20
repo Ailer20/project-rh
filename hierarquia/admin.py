438df53bb2efb3d8165ec13cd33a8cefee5f4b04
Ailer20 <aronvieira820@gmail.com>
2025-10-20 12:31:21 -0400

    add de funcionarios teste

--- hierarquia/admin.py
+++ hierarquia/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# Importe os modelos REAIS do seu models.py
+from .models import Cargo, Setor, CentroServico, Funcionario, Requisicao
 
-# Register your models here.
+# Registre-os
+admin.site.register(Cargo)
+admin.site.register(Setor)
+admin.site.register(CentroServico)
+admin.site.register(Funcionario)
+admin.site.register(Requisicao)
